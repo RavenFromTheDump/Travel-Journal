184252a8027a7c8310d97f3137ace13965ea68bd
RavenFromTheDump <osuname848@gmail.com>
2023-01-29 14:27:46 +0100

    Added link to database

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export default [
         title: 'Marseille',
         date: '02 Sep,2022 - 09 Sep,2022',
         img: '/marseille.jpg',
-        googleMap: '',
+        googleMap: 'https://goo.gl/maps/mnr52rFkhvZkVzAD7',
         text: 'Marseille, a port city in southern France, has been a crossroads of immigration and trade since its founding by the Greeks circa 600 B.C. At its heart is the Vieux-Port (Old Port), where fishmongers sell their catch along the boat-lined quay.',
     },
     {
@@ -14,7 +14,7 @@ export default [
         title: 'Eibsee',
         date: '23 Aug,2022 - 24 Aug,2022',
         img: '/eibsee.jpg',
-        googleMap: '',
+        googleMap: 'https://goo.gl/maps/rW2BMPDWGoPR8h8eA',
         text: 'Eibsee is a lake in Bavaria, Germany. It is at the northerly base of the Zugspitze, Germany\'s highest mountain.',
     },
     {
@@ -23,7 +23,7 @@ export default [
         title: 'Rome',
         date: '23 Aug,2022 - 24 Aug,2022',
         img: '/rome.jpg',
-        googleMap: '',
+        googleMap: 'https://goo.gl/maps/jY3Gmi7m5Gp8g7Yv8',
         text: 'Rome',
     }
 ]
